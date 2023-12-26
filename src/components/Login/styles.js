b88df16de43d2529abe07d6f805019ce8036f40a
Ganesh651 <ganireddy651@gmail.com>
2023-12-26 11:04:56 +0530

    login page styling chnages

--- src/components/Login/styles.js
+++ src/components/Login/styles.js
@@ -14,7 +14,7 @@ export const LoginContainer = styled.div`
   margin: auto;
   @media screen and (max-width: 768px){
     margin: 0;
-    width: 100%;
+    width: 89%;
     padding: 20px;
     flex-direction: column;
     justify-content: flex-start;
@@ -24,13 +24,13 @@ export const LoginContainer = styled.div`
 export const LoginForm = styled.form`
   background-color: #FDFEFF;
   box-shadow: 0px 4px 16px 0px #bfbfbf;
-  padding: 30px;
+  padding: 20px;
   width: 350px;
   border-radius: 5px;
   @media screen and (max-width: 768px){
   background-color: transparent;
   box-shadow: none;
-  width: 95%;
+  width: 100%;
   padding: 0;
 }
 `
@@ -70,6 +70,7 @@ export const LoginText = styled.h2`
 `
 export const InputContainer = styled.div`
   margin-bottom: 20px;
+  width: 99%
 `
 export const Label = styled.label`
   color: #475569;
@@ -88,12 +89,12 @@ export const Input = styled.input`
 `
 export const ErrorMessage = styled.p`
   color: #EF4444;
-  font-size: 10px;
+  font-size: 13px;
   font-family: Bree Serif;
 `
 
 export const LoginButton = styled.button`
-  width: 99%;
+  width: 100%;
   color: #fff;
   border: 0;
   outline: none;
@@ -108,8 +109,9 @@ export const LoginImageContainer = styled.div`
   }
 `
 export const LoginImage = styled.img`
-    height: 600px;
-    width: 570px
+    height: auto;
+    width: 100%;
+    max-width: 100%;
 `
 
 export const LoginImageMobile = styled.img`
@@ -117,8 +119,8 @@ export const LoginImageMobile = styled.img`
     display: none;
   }
   height: 20%;
-  width: 100%;
-  margin-left: 30px;
+  width: 95%;
+  padding-left: 30px;
 `
 
 export const LoadingContainer = styled.div`
